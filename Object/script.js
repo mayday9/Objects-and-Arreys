c3aec856b9c4890cb3a9be267d38194f4f8e2514
mayday9 <themaydaygmt@gmail.com>
2018-08-16 17:25:28 +0300

    Loading homework "Object"

--- Object/script.js
+++ Object/script.js
@@ -1,3 +1,55 @@
-var = {
-    
+var cellPhone = {
+    model: "Redmi Note 5",
+    brend: "Xiaomi",
+    year: 2018,
+    cpu: "Qualcom",
+    cpuCores: 8,
+    memory: 32,
+    gpu: "Adreno 509",
+    system: "miui 9.5",
+    fingerprint: true,
+    displaySize: 6,
+
+    ring: function () {
+        return "Dziiiiiinnnnn!!!!";
+    },
+    sms: function () {
+        return "Dilin-Dilin!!"
+    },
+    interNet: function () {
+        return "2G, 3G, 4G all working"
+    },
+    aboutSellPhone: function () {
+        console.log(this.model, this.brend, this.year);
+    },
+    gpuInfo: function () {
+        console.log(this.cpu, this.cpuCores);
+    },
+    isItNewModel: function () {
+        if(this.year >= 2018){
+            return true;
+        } else {return false;}
+    },
+    largeDisplay: function () {
+        if(this.displaySize >=5) {
+            return "large!";
+        } else if (this.displaySize < 5 && this.displaySize >= 4) {
+            return "middle!";
+        } else {return "small"}
+    },
+    fingerprintStat: function () {
+        return "Is fingerprint a cool finction? No, это понты для приезжих!";
+    },
+    memVersion: function () {
+        return "Exists 32 and 64MB versions";
+    },
+    guarantee: function () {
+        return "Pay more 30 dollars and You'll get guarantee"
+    },
+
 };
+
+// console.log(cellPhone.ring());
+// cellPhone.aboutSellPhone();
+// console.log(cellPhone.isItNewModel());
+// console.log(cellPhone.largeDisplay());
